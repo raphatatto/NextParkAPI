3cf13233f5ddc16cf848c7980c8ec171058745ce
Raphaela Oliveira Tatto <143215478+raphatatto@users.noreply.github.com>
2025-09-29 13:58:33 -0300

    docs: enrich swagger documentation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using NextParkAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +11,34 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<NextParkContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("OracleDb")));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "NextPark API",
+        Version = "v1",
+        Description = "API para gestão de motos, vagas e manutenções em um pátio de estacionamento, incluindo paginação e hipermídia.",
+        Contact = new OpenApiContact
+        {
+            Name = "NextPark",
+            Email = "contato@nextpark.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+
+    options.EnableAnnotations();
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+    }
+});
 
 builder.WebHost.UseUrls("http://0.0.0.0:80");
 
